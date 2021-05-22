fbe7d8aa8eda654c17aaa60f81133c24297b2c1e
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2021-05-22 12:25:06 -0700

    Ex 4.6*: helper functions and unit tests, step4

--- submissions/blog-list/tests/list_helper.test.js
+++ submissions/blog-list/tests/list_helper.test.js
@@ -127,3 +127,147 @@ describe('favorite blog - most likes', () => {
         )
     })
 })
+
+describe('most blogs', () => {
+
+    //test - only 1 blog, total
+    test('one blog', () => {
+        const result = listHelper.mostBlogs(listWithOneBlog)
+        expect(result).toEqual(
+            {
+                author: 'Edsger W. Dijkstra', 
+                blogs: 1
+            }
+        )
+    })
+
+    const manyBlogsSameAuthor = [
+        {
+            _id: '5a422a851b54a676234d17f7',
+            title: 'React patterns',
+            author: 'Michael Chan',
+            url: 'https://reactpatterns.com/',
+            likes: 7,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Michael Chan',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422b3a1b54a676234d17f9',
+            title: 'Canonical string reduction',
+            author: 'Michael Chan',
+            url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+            likes: 12,
+            __v: 0
+        },
+        {
+            _id: '5a422b891b54a676234d17fa',
+            title: 'First class tests',
+            author: 'Michael Chan',
+            url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+            likes: 10,
+            __v: 0
+        },
+        {
+            _id: '5a422ba71b54a676234d17fb',
+            title: 'TDD harms architecture',
+            author: 'Michael Chan',
+            url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+            likes: 0,
+            __v: 0
+        },
+        {
+            _id: '5a422bc61b54a676234d17fc',
+            title: 'Type wars',
+            author: 'Michael Chan',
+            url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+            likes: 2,
+            __v: 0
+        }  
+    ]
+
+    //test - many blogs, all written by the same author
+    test('many blogs same author', () => {
+        const result = listHelper.mostBlogs(manyBlogsSameAuthor)
+        expect(result).toEqual({
+            author: 'Michael Chan', 
+            blogs: 6
+        })
+    })
+
+    //test - many blogs, many author, some with equal number of blogs - just return one
+    const manyBlogsManyAuthorsWithSameAmountPublished = [
+        {
+            _id: '5a422a851b54a676234d17f7',
+            title: 'React patterns',
+            author: 'Michael Chan',
+            url: 'https://reactpatterns.com/',
+            likes: 7,
+            __v: 0
+        },
+        {
+            _id: '5a422aa71b54a676234d17f8',
+            title: 'Go To Statement Considered Harmful',
+            author: 'Michael Chan',
+            url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+            likes: 5,
+            __v: 0
+        },
+        {
+            _id: '5a422b3a1b54a676234d17f9',
+            title: 'Canonical string reduction',
+            author: 'Michael Chan',
+            url: 'http://www.cs.utexas.edu/~EWD/transcriptions/EWD08xx/EWD808.html',
+            likes: 12,
+            __v: 0
+        },
+        {
+            _id: '5a422b891b54a676234d17fa',
+            title: 'First class tests',
+            author: 'Robert C. Martin',
+            url: 'http://blog.cleancoder.com/uncle-bob/2017/05/05/TestDefinitions.htmll',
+            likes: 10,
+            __v: 0
+        },
+        {
+            _id: '5a422ba71b54a676234d17fb',
+            title: 'TDD harms architecture',
+            author: 'Robert C. Martin',
+            url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+            likes: 0,
+            __v: 0
+        },
+        {
+            _id: '5a422bc61b54a676234d17fc',
+            title: 'Type wars',
+            author: 'Robert C. Martin',
+            url: 'http://blog.cleancoder.com/uncle-bob/2016/05/01/TypeWars.html',
+            likes: 2,
+            __v: 0
+        }  
+    ]
+
+    test('many blogs many authors with same number of blogs', () => {
+        const result = listHelper.mostBlogs(manyBlogsManyAuthorsWithSameAmountPublished)
+        expect(result).toEqual({
+            author: 'Michael Chan',
+            blogs: 3
+        })
+    })
+
+    //test - many blogs, many authors, one with more than the others
+    test('many blogs, many authors, one author has more than the others', () => {
+        const result = listHelper.mostBlogs(manyBlogs)
+        expect(result).toEqual({
+            author: 'Robert C. Martin',
+            blogs: 3
+        })
+        
+    }) 
+})
--- submissions/blog-list/utils/list_helper.js
+++ submissions/blog-list/utils/list_helper.js
@@ -1,3 +1,5 @@
+var _ = require('lodash')
+
 const dummy = (blogs) => {
     return 1
 }
@@ -22,9 +24,42 @@ const favoriteBlog = (blogs) => {
 
     return favouriteBlog
 }
+//Define a function called mostBlogs that receives an array of blogs as a parameter. 
+//The function returns the author who has the largest amount of blogs. 
+//The return value also contains the number of blogs the top author has:
+
+const mostBlogs = (blogs) => {
+    const authorsArray = _.uniq(blogs.map(b => b.author))
+    var authors = []
+    for (var i=0; i < authorsArray.length; i++) {
+        authors.push({ author: authorsArray[i], blogs: 0})
+    }
+
+    //loop through array of blogs, this time incrementing the author's numBlogs in the authors object each time we come across a blog by them
+    for (var j=0; j < blogs.length; j++) {
+        //for each blog, find the author who wrote it and increment his blogs in the object
+        for (var k=0; k <authors.length; k++) {
+            if (authors[k].author === blogs[j].author) {
+                //console.log('Found the author who wrote', blogs[j], 'his names are: ', blogs[j].author, authors[k].author)
+                authors[k].blogs = authors[k].blogs += 1
+            }
+        }        
+    }
+    //console.log(authors)
+    //loop through authors object, storing author with most blogs seen so far, and returning that by the end of it
+    var mostBlogsAuthor = authors[0]
+    for (var l=1; l < authors.length; l++) {
+        if (authors[l].blogs > mostBlogsAuthor.blogs) {
+            mostBlogsAuthor = authors[l]
+        }
+    }
+    return mostBlogsAuthor
+
+}
 
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs
 }
