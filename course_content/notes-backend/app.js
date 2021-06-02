8e3d2c908d3cc8edd8f834b2b6af1448fddcf737
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2021-06-01 17:19:12 -0700

    4b before exercises

--- course_content/notes-backend/app.js
+++ course_content/notes-backend/app.js
@@ -1,5 +1,6 @@
 const config = require('./utils/config')
 const express = require('express')
+require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const notesRouter = require('./controllers/notes')
@@ -10,12 +11,12 @@ const mongoose = require('mongoose')
 logger.info('connecting to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-  .then(() => {
-    logger.info('connected to MongoDB')
-  })
-  .catch((error) => {
-    logger.error('error connecting to MongoDB:', error.message)
-  })
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
 
 app.use(cors())
 app.use(express.static('build'))
