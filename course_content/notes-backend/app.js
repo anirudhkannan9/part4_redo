491ef9434f893ec070aa7eb46c00cf55e9b2b68f
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2021-06-02 23:06:48 -0700

    Changes at the end of part4c

--- course_content/notes-backend/app.js
+++ course_content/notes-backend/app.js
@@ -4,6 +4,7 @@ require('express-async-errors')
 const app = express()
 const cors = require('cors')
 const notesRouter = require('./controllers/notes')
+const usersRouter = require('./controllers/users')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -25,6 +26,7 @@ app.use(middleware.requestLogger)
 
 //notesRouter only defines relative routes because we define the 'absolute'/base route when taking it into use here
 app.use('/api/notes', notesRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- course_content/notes-backend/tests/note_api.test.js
+++ course_content/notes-backend/tests/note_api.test.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const supertest = require('supertest')
+const bcrypt = require('bcrypt')
 const helper = require('./test_helper')
 //import Express application
 const app = require('../app')
@@ -8,6 +9,8 @@ const app = require('../app')
 //supertests ensures that app being tested is started at the port that it uses internally
 const api = supertest(app)
 const Note = require ('../models/note')
+const User = require('../models/user')
+
 
 beforeEach(async () => {
     await Note.deleteMany({})
@@ -136,6 +139,60 @@ describe('deletion of a note', () => {
     
         expect(contents).not.toContain(noteToDelete.content)
     
+    })
+})
+
+describe('when there is initially one user in db', () => {
+    beforeEach(async () => {
+        await User.deleteMany({})
+
+        const passwordHash = await bcrypt.hash('sekret', 10)
+        const user = new User({ username: 'root', passwordHash})
+
+        await user.save()
+    })
+
+    test('creation succeeds with a fresh username', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'kannana1',
+            name: 'Anirudh Kannan',
+            password: 'secret'
+        }
+
+        await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(200)
+            .expect('Content-Type', /application\/json/)
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+        const usernames = usersAtEnd.map(u => u.username)
+        expect(usernames).toContain(newUser.username)
+    })
+
+    test('creation fails with proper statuscode and message if username already taken', async () => {
+        const usersAtStart = await helper.usersInDb()
+
+        const newUser = {
+            username: 'root',
+            name: 'Superuser',
+            password: 'secret'
+        }
+
+        const result = await api
+            .post('/api/users')
+            .send(newUser)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
+
+        expect(result.body.error).toContain('`username` to be unique')
+
+        const usersAtEnd = await helper.usersInDb()
+        expect(usersAtEnd).toHaveLength(usersAtStart.length)
     })
 })
 
