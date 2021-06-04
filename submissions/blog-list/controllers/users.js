7b47825f408e5b5475b4360ef1ba324b67929bee
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2021-06-03 19:05:45 -0700

    Ex4.16: bloglist expansion, step 4

--- submissions/blog-list/controllers/users.js
+++ submissions/blog-list/controllers/users.js
@@ -11,6 +11,12 @@ usersRouter.get('/', async (request, response) => {
 usersRouter.post('/', async (request, response) => {
     const body = request.body
 
+    if (!body.password || body.password.isUndefined || body.password === null) {
+        return response.status(400).json({ error: 'password missing; please provide a password with the request' })
+    } else if (body.password.length < 3) {
+        return response.status(400).json({ error: 'password must be at least 3 characters long' })
+    }
+
     const saltRounds = 10
     const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
