b5269212eced5d83aaa93b7c0707a07e733a8438
anirudhkannan9 <47587789+anirudhkannan9@users.noreply.github.com>
2021-06-03 19:23:43 -0700

    Ex4.17: bloglist expansion, step 5

--- submissions/blog-list/controllers/blogs.js
+++ submissions/blog-list/controllers/blogs.js
@@ -1,8 +1,9 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
     response.json(blogs)
 })
 
@@ -21,14 +22,21 @@ blogsRouter.post('/', async (request, response) => {
     if (body.title === undefined || body.url === undefined) {
         response.send(400).end()
     } else {
+        const user = await User.findOne({})
+
+       
         const blog = new Blog({
             title: body.title,
             author: body.author,
             url: body.url,
-            likes: body.likes === undefined ? 0 : body.likes
+            likes: body.likes === undefined ? 0 : body.likes,
+            user: user._id
         })
     
         const savedBlog = await blog.save()
+        user.blogs = user.blogs.concat(savedBlog._id)
+        await user.save()
+
         response.json(savedBlog)
     }
 })
